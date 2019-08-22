0dc18b344e86b67f3d85ad1038a6985c7b21d489
Futuristic Coder <FuturistiCoder@gmail.com>
2019-08-22 16:59:54 +0200

    Add Unsubscriber Test

---
+++ LiteDB.Realtime.Test/Subscriptions/Unsubscriber_Should.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using LiteDB.Realtime.Subscriptions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LiteDB.Realtime.Test.Subscriptions
+{
+    using SubscriptionDict = ConcurrentDictionary<ISubscription, byte>;
+    public class Unsubscriber_Should : SubscriptionBuilderTestBase
+    {
+        class Model { }
+
+        [Fact]
+        public void Unsubscribe_From_DB_When_Disposing()
+        {
+            var subscriptions = new SubscriptionDict();
+            var sub1 = new Subscription<Model>(_db);
+            var sub2 = new Subscription<Model>(_db);
+            subscriptions.TryAdd(sub1, default);
+            subscriptions.TryAdd(sub2, default);
+            subscriptions.Keys.Should().HaveCount(2);
+            subscriptions.Keys.Should().Contain(sub1);
+            subscriptions.Keys.Should().Contain(sub2);
+
+            using (var unsubscriber = new Unsubscriber(subscriptions, sub1))
+            { }
+
+            subscriptions.Keys.Should().HaveCount(1);
+            subscriptions.Keys.Should().Contain(sub2);
+        }
+    }
+}
