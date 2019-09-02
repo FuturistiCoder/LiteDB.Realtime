80bc02ca46820583c2986e03db75ef09fb8603c1
Futuristic Coder <FuturistiCoder@gmail.com>
2019-09-02 11:41:04 +0200

    Should Notify Document When A New Data Added

--- LiteDB.Realtime.Test/Database/RealtimeLiteDatabase_Should.cs
+++ LiteDB.Realtime.Test/Database/RealtimeLiteDatabase_Should.cs
@@ -17,6 +17,7 @@ namespace LiteDB.Realtime.Test.Database
             public decimal Price { get; set; }
 
         }
+
         [Fact]
         public void Be_Created_By_Constructors()
         {
@@ -36,150 +37,189 @@ namespace LiteDB.Realtime.Test.Database
         [Fact]
         public void Notify_Collection_Subscription_When_A_New_Data_Added()
         {
-            using (var db = new RealtimeLiteDatabase(new MemoryStream()))
+            using var db = new RealtimeLiteDatabase(new MemoryStream());
+            List<Item> receivedItems = null;
+            // collection subscription
+            db.Realtime.Collection<Item>("items").Subscribe(items => receivedItems = items);
+
+            //waiting for notification
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            receivedItems.Should().BeEmpty();
+
+            var newItem = new Item
             {
-                List<Item> receivedItems = null;
-                // collection subscription
-                db.Realtime.Collection<Item>("items").Subscribe(items => receivedItems = items);
+                Name = "Keyboard",
+                Price = 100m
+            };
+            var newId = db.GetCollection<Item>("items").Insert(newItem);
+            newId.IsGuid.Should().BeTrue();
 
-                //waiting for notification
-                Thread.Sleep(TimeSpan.FromSeconds(1));
-                receivedItems.Should().BeEmpty();
+            // waiting for notification
+            Thread.Sleep(TimeSpan.FromSeconds(1));
 
-                var newItem = new Item
-                {
-                    Name = "Keyboard",
-                    Price = 100m
-                };
-                var newId = db.GetCollection<Item>("items").Insert(newItem);
-                newId.IsGuid.Should().BeTrue();
-
-                // waiting for notification
-                Thread.Sleep(TimeSpan.FromSeconds(1));
-
-                receivedItems.Should().NotBeNull();
-                receivedItems.Should().HaveCount(1);
-                receivedItems[0].Id.Should().Be(newId.AsGuid);
-                receivedItems[0].Name.Should().Be(newItem.Name);
-                receivedItems[0].Price.Should().Be(newItem.Price);
-            }
+            receivedItems.Should().NotBeNull();
+            receivedItems.Should().HaveCount(1);
+            receivedItems[0].Id.Should().Be(newId.AsGuid);
+            receivedItems[0].Name.Should().Be(newItem.Name);
+            receivedItems[0].Price.Should().Be(newItem.Price);
         }
 
         [Fact]
         public void Notify_Docuemnt_And_Collection_Subscription_When_The_Document_Modified()
         {
-            using (var db = new RealtimeLiteDatabase(new MemoryStream()))
+            using var db = new RealtimeLiteDatabase(new MemoryStream());
+            Item receivedItem = null;
+            List<Item> receivedItems = null;
+            var newItem = new Item
             {
-                Item receivedItem = null;
-                List<Item> receivedItems = null;
-                var newItem = new Item
-                {
-                    Name = "Keyboard",
-                    Price = 100m
-                };
-                var newId = db.GetCollection<Item>("items").Insert(newItem);
-                newItem.Id = newId.AsGuid;
+                Name = "Keyboard",
+                Price = 100m
+            };
+            var newId = db.GetCollection<Item>("items").Insert(newItem);
+            newItem.Id = newId.AsGuid;
 
-                // docuement subscription
-                db.Realtime.Collection<Item>("items").Id(newId).Subscribe(item => receivedItem = item);
+            // docuement subscription
+            db.Realtime.Collection<Item>("items").Id(newId).Subscribe(item => receivedItem = item);
 
-                // collection subscription
-                db.Realtime.Collection<Item>("items").Subscribe(items => receivedItems = items);
+            // collection subscription
+            db.Realtime.Collection<Item>("items").Subscribe(items => receivedItems = items);
 
-                // waiting for notification
-                Thread.Sleep(TimeSpan.FromSeconds(1));
+            // waiting for notification
+            Thread.Sleep(TimeSpan.FromSeconds(1));
 
-                // document subscription received
-                receivedItem.Should().NotBeNull();
-                receivedItem.Id.Should().Be(newItem.Id);
-                receivedItem.Name.Should().Be(newItem.Name);
-                receivedItem.Price.Should().Be(newItem.Price);
+            // document subscription received
+            receivedItem.Should().NotBeNull();
+            receivedItem.Id.Should().Be(newItem.Id);
+            receivedItem.Name.Should().Be(newItem.Name);
+            receivedItem.Price.Should().Be(newItem.Price);
 
-                // collection subscription received
-                receivedItems.Should().NotBeNull();
-                receivedItems.Should().HaveCount(1);
-                receivedItems[0].Id.Should().Be(newItem.Id);
-                receivedItems[0].Name.Should().Be(newItem.Name);
-                receivedItems[0].Price.Should().Be(newItem.Price);
+            // collection subscription received
+            receivedItems.Should().NotBeNull();
+            receivedItems.Should().HaveCount(1);
+            receivedItems[0].Id.Should().Be(newItem.Id);
+            receivedItems[0].Name.Should().Be(newItem.Name);
+            receivedItems[0].Price.Should().Be(newItem.Price);
 
-                // updating newItem
-                newItem.Price = 99m;
-                db.GetCollection<Item>("items").Update(newItem);
+            // updating newItem
+            newItem.Price = 99m;
+            db.GetCollection<Item>("items").Update(newItem);
 
-                // waiting for notification
-                Thread.Sleep(TimeSpan.FromSeconds(1));
+            // waiting for notification
+            Thread.Sleep(TimeSpan.FromSeconds(1));
 
-                // document subscription received
-                receivedItem.Should().NotBeNull();
-                receivedItem.Id.Should().Be(newItem.Id);
-                receivedItem.Name.Should().Be(newItem.Name);
-                receivedItem.Price.Should().Be(newItem.Price);
+            // document subscription received
+            receivedItem.Should().NotBeNull();
+            receivedItem.Id.Should().Be(newItem.Id);
+            receivedItem.Name.Should().Be(newItem.Name);
+            receivedItem.Price.Should().Be(newItem.Price);
 
-                // collection subscription received
-                receivedItems.Should().NotBeNull();
-                receivedItems.Should().HaveCount(1);
-                receivedItems[0].Id.Should().Be(newItem.Id);
-                receivedItems[0].Name.Should().Be(newItem.Name);
-                receivedItems[0].Price.Should().Be(newItem.Price);
-            }
+            // collection subscription received
+            receivedItems.Should().NotBeNull();
+            receivedItems.Should().HaveCount(1);
+            receivedItems[0].Id.Should().Be(newItem.Id);
+            receivedItems[0].Name.Should().Be(newItem.Name);
+            receivedItems[0].Price.Should().Be(newItem.Price);
         }
 
         [Fact]
-        public void Notify_Docuemnt_And_Collection_Subscription_When_Broadcasting()
+        public void Notify_Document_And_Collection_Subscription_When_Broadcasting()
         {
-            using (var db = new RealtimeLiteDatabase(new MemoryStream()))
+            using var db = new RealtimeLiteDatabase(new MemoryStream());
+            Item receivedItem = null;
+            List<Item> receivedItems = null;
+            var newItem = new Item
             {
-                Item receivedItem = null;
-                List<Item> receivedItems = null;
-                var newItem = new Item
-                {
-                    Name = "Keyboard",
-                    Price = 100m
-                };
-                var newId = db.GetCollection<Item>("items").Insert(newItem);
-                newItem.Id = newId.AsGuid;
+                Name = "Keyboard",
+                Price = 100m
+            };
+            var newId = db.GetCollection<Item>("items").Insert(newItem);
+            newItem.Id = newId.AsGuid;
 
-                // docuement subscription
-                db.Realtime.Collection<Item>("items").Id(newId).Subscribe(item => receivedItem = item);
-                // collection subscription
-                db.Realtime.Collection<Item>("items").Subscribe(items => receivedItems = items);
+            // docuement subscription
+            db.Realtime.Collection<Item>("items").Id(newId).Subscribe(item => receivedItem = item);
+            // collection subscription
+            db.Realtime.Collection<Item>("items").Subscribe(items => receivedItems = items);
 
-                // waiting for notification
-                Thread.Sleep(TimeSpan.FromSeconds(1));
+            // waiting for notification
+            Thread.Sleep(TimeSpan.FromSeconds(1));
 
-                // document subscription received
-                receivedItem.Should().NotBeNull();
-                receivedItem.Id.Should().Be(newItem.Id);
-                receivedItem.Name.Should().Be(newItem.Name);
-                receivedItem.Price.Should().Be(newItem.Price);
+            // document subscription received
+            receivedItem.Should().NotBeNull();
+            receivedItem.Id.Should().Be(newItem.Id);
+            receivedItem.Name.Should().Be(newItem.Name);
+            receivedItem.Price.Should().Be(newItem.Price);
 
-                // collection subscription received
-                receivedItems.Should().NotBeNull();
-                receivedItems.Should().HaveCount(1);
-                receivedItems[0].Id.Should().Be(newItem.Id);
-                receivedItems[0].Name.Should().Be(newItem.Name);
-                receivedItems[0].Price.Should().Be(newItem.Price);
+            // collection subscription received
+            receivedItems.Should().NotBeNull();
+            receivedItems.Should().HaveCount(1);
+            receivedItems[0].Id.Should().Be(newItem.Id);
+            receivedItems[0].Name.Should().Be(newItem.Name);
+            receivedItems[0].Price.Should().Be(newItem.Price);
 
-                // update with expression (will broadcast)
-                int updatedNum = db.GetCollection<Item>("items").UpdateMany(item => new Item { Id = item.Id, Name = item.Name, Price = item.Price * 2 }, i => i.Price > 0);
-                updatedNum.Should().Be(1);
+            // update with expression (will broadcast)
+            int updatedNum = db.GetCollection<Item>("items").UpdateMany(item => new Item { Id = item.Id, Name = item.Name, Price = item.Price * 2 }, i => i.Price > 0);
+            updatedNum.Should().Be(1);
 
-                // waiting for notification
-                Thread.Sleep(TimeSpan.FromSeconds(1));
+            // waiting for notification
+            Thread.Sleep(TimeSpan.FromSeconds(1));
 
-                // document subscription received
-                receivedItem.Should().NotBeNull();
-                receivedItem.Id.Should().Be(newId.AsGuid);
-                receivedItem.Name.Should().Be(newItem.Name);
-                receivedItem.Price.Should().Be(newItem.Price * 2);
+            // document subscription received
+            receivedItem.Should().NotBeNull();
+            receivedItem.Id.Should().Be(newId.AsGuid);
+            receivedItem.Name.Should().Be(newItem.Name);
+            receivedItem.Price.Should().Be(newItem.Price * 2);
 
-                // collection subscription received
-                receivedItems.Should().NotBeNull();
-                receivedItems.Should().HaveCount(1);
-                receivedItems[0].Id.Should().Be(newId.AsGuid);
-                receivedItems[0].Name.Should().Be(newItem.Name);
-                receivedItems[0].Price.Should().Be(newItem.Price * 2);
-            }
+            // collection subscription received
+            receivedItems.Should().NotBeNull();
+            receivedItems.Should().HaveCount(1);
+            receivedItems[0].Id.Should().Be(newId.AsGuid);
+            receivedItems[0].Name.Should().Be(newItem.Name);
+            receivedItems[0].Price.Should().Be(newItem.Price * 2);
+        }
+
+        [Fact]
+        public void Notify_Null_Document_If_Id_Does_Not_Exist_When_Subscribing()
+        {
+            using var db = new RealtimeLiteDatabase(new MemoryStream());
+            bool isNull = false;
+            db.Realtime.Collection<Item>("items").Id(new BsonValue(Guid.NewGuid())).Subscribe(item => isNull = item is null);
+
+            // waiting for notification
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+
+            isNull.Should().BeTrue();
+        }
+
+
+        [Fact]
+        public void Notify_Document_When_A_New_Data_Added()
+        {
+            var bsonvalue = new BsonValue();
+            var id = Guid.NewGuid();
+            using var db = new RealtimeLiteDatabase(new MemoryStream());
+            Item receivedItem = new Item();
+            db.Realtime.Collection<Item>("items").Id(new BsonValue(id)).Subscribe(item => receivedItem = item);
+
+            // waiting for notification
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+
+            receivedItem.Should().BeNull();
+
+            var item = new Item
+            {
+                Id = id,
+                Name = "Mouse",
+                Price = 10m
+            };
+
+            db.GetCollection<Item>("items").Insert(item);
+
+            // waiting for notification
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+
+            receivedItem.Id.Should().Be(item.Id);
+            receivedItem.Name.Should().Be(item.Name);
+            receivedItem.Price.Should().Be(item.Price);
         }
     }
 }
